f766031a518cdcc17a76b501af08f75a3c97cb59
Sjors Provoost <sjors@sprovoost.nl>
2013-05-17 13:04:05 +0200

    Empty Rails 4.0 project using postgres

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BitcoinPassbook::Application.config.secret_key_base = 'af178384eb2a8eb0c9ff19cf006d926ed5aeb6edd005a6068fa440d2efe851a05326143d603de158c65c2e7946ac77b3a92dadc637fafbea67b48f20c121e600'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BitcoinPassbook::Application.config.session_store :cookie_store, key: '_bitcoin-passbook_session'
