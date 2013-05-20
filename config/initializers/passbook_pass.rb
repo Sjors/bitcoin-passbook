f098a7b5a246691ad21e810aa04a6661e7c5219e
Sjors Provoost <sjors@sprovoost.nl>
2013-05-20 14:25:40 +0200

    Don't configure pass certificate in Test environment

--- config/initializers/passbook_pass.rb
+++ config/initializers/passbook_pass.rb
@@ -1,8 +1,10 @@
-Passbook::Config.instance.add_pkpass do |passbook|
-  passbook.pass_config["pass.com.bitcoin-passbook.address"]={
-                              "cert_path"=>"config/pass-address.p12",
-                              "cert_password"=> ENV["PASS_ADDRESS"],
-                              "template_path"=>"data/templates/pass.com.bitcoin-passbook.address",
-                              "class"=>"Pass"
-                            }
+if Rails.env != "test"
+  Passbook::Config.instance.add_pkpass do |passbook|
+    passbook.pass_config["pass.com.bitcoin-passbook.address"]={
+                                "cert_path"=>"config/pass-address.p12",
+                                "cert_password"=> ENV["PASS_ADDRESS"],
+                                "template_path"=>"data/templates/pass.com.bitcoin-passbook.address",
+                                "class"=>"Pass"
+                              }
+  end
 end
