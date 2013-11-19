795dacb9d6e4e7ded9e498bcc21cb5c4680a84f2
Sjors Provoost <sjors@sprovoost.nl>
2013-11-19 19:57:10 +0800

    Dynamic price

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -15,7 +15,7 @@ class AddressesController < ApplicationController
       unless Rails.env == "test"
         begin
           coinbase = Coinbase::Client.new(ENV['COINBASE_API_KEY'])
-          button = coinbase.create_button "Your Order ##{ @address.id }", 0.01, "1 pass for your #{ @address.name } address", "A#{ @address.id }"
+          button = coinbase.create_button "Your Order ##{ @address.id }", ENV['PRICE'], "1 pass for your #{ @address.name } address", "A#{ @address.id }"
           @address.update button_code: button.button.code
       
           @address.fetch_balance_and_last_transaction!
