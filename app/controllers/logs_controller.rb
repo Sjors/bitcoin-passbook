a5825c866f3cd87a187dcc77f447446c102d34cc
Sjors Provoost <sjors@sprovoost.nl>
2014-08-13 21:26:56 +0200

    Write log to log

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -3,13 +3,15 @@ class LogsController < ApplicationController
   skip_before_filter :verify_authenticity_token, :only => [:log]
 
   def log
-    # if request && request.body
-    #   request.body.rewind
-    #   json_body = JSON.parse(request.body.read)
-    #   File.open(File.expand_path(Rails.root) + "/log/devices.log", "a") do |f|
-    #     f.write "[#{Time.now}] #{json_body["description"]}\n"
-    #   end
-    # end
+    if request && request.body
+      request.body.rewind
+      json_body = JSON.parse(request.body.read)
+      # File.open(File.expand_path(Rails.root) + "/log/devices.log", "a") do |f|
+  #       f.write "[#{Time.now}] #{json_body["description"]}\n"
+  #  end
+      logger.error json_body["description"]
+    end
+    
       render :nothing => true, :status => 200
   end
 
