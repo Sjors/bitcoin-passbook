3c49ec1401f4e50c7be886c7dbc5ab01ba2e042d
Sjors Provoost <sjors@sprovoost.nl>
2015-01-01 15:24:21 +0100

    Remove Urban Airship initializer

--- config/initializers/urban_airship.rb
+++
@@ -1,5 +0,0 @@
-Urbanairship.application_key = 'wr4we_yiTBmTQ_8f7uZrlw'
-Urbanairship.application_secret = ENV['AIRSHIP_APPLICATION_SECRET']
-Urbanairship.master_secret = ENV['AIRSHIP_MASTER_SECRET']
-Urbanairship.logger = Rails.logger
-Urbanairship.request_timeout = 5 # default
